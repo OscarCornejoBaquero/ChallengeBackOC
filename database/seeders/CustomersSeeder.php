f1fda208812a0c8f0bc75b938dac4f1d848fd078
Oscar-1989 <oscar.cornejob@ug.edu.ec>
2022-03-12 15:47:20 -0500

    Creación de Seeders

--- database/seeders/CustomersSeeder.php
+++ database/seeders/CustomersSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CustomersSeeder extends Seeder
 {
@@ -14,6 +15,42 @@ class CustomersSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('customers')->insert([
+            [
+                'first_name' => 'Oscar Leonardo',
+                'last_name' => 'Cornejo Baquero',
+                'identification_card' => '0927218487',
+                'email' => 'oscar.corneb@gmail.com',
+                'cell_phone' => '0981814691'
+            ],
+            [
+                'first_name' => 'Saskya Esperanza',
+                'last_name' => 'Zambrano Naranjo',
+                'identification_card' => '0957218487',
+                'email' => 'saskya.corneb@gmail.com',
+                'cell_phone' => '0982134691'
+            ],
+            [
+                'first_name' => 'Isabella Sasha',
+                'last_name' => 'Cornejo Zambrano',
+                'identification_card' => '0927218457',
+                'email' => 'isa.bella@gmail.com',
+                'cell_phone' => '0981554691'
+            ],
+            [
+                'first_name' => 'Angela Edilma',
+                'last_name' => 'Baquero Romo',
+                'identification_card' => '0925258487',
+                'email' => 'angela.baque@gmail.com',
+                'cell_phone' => '0981814111'
+            ],
+            [
+                'first_name' => 'Pro Cat',
+                'last_name' => 'Fito Colorito',
+                'identification_card' => '0988218487',
+                'email' => 'fito@gmail.com',
+                'cell_phone' => '0912814691'
+            ]
+        ]);
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -14,6 +15,37 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        
+        DB::table('products')->insert([
+            [
+                'product_name' => 'Impresora',
+                'product_description' => 'Epson L355',
+                'product_stock' => 100,
+                'product_price' => 210.50
+            ],
+            [
+                'product_name' => 'Laptop',
+                'product_description' => 'HP 14La001',
+                'product_stock' => 10,
+                'product_price' => 550.10
+            ],
+            [
+                'product_name' => 'Celular',
+                'product_description' => 'Redmi 200',
+                'product_stock' => 5,
+                'product_price' => 150.50
+            ],
+            [
+                'product_name' => 'Monitor',
+                'product_description' => 'Lg-19.5 pulgadas',
+                'product_stock' => 99,
+                'product_price' => 112.10
+            ],
+            [
+                'product_name' => 'Televisor Sony',
+                'product_description' => '23 pulgadas Android',
+                'product_stock' => 19,
+                'product_price' => 780.50
+            ]
+        ]);
     }
 }
